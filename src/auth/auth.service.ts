eca89a74898bb14f120e997766a282c1396b60d4
Hubert <hubert.zajac92@gmail.com>
2022-08-23 08:54:42 +0200

    Add service methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UsersEntity } from './user.entity';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @InjectRepository(UsersEntity) private usersRepository: Repository<UsersEntity>,
+        private jwtService: JwtService,
+    ) { }
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+
+        const user = this.usersRepository.create({ username, password: hashedPassword });
+
+        try {
+            await this.usersRepository.save(user);
+        } catch (error) {
+            if (error.code === 'ER_DUP_ENTRY') {
+                throw new ConflictException('Username already exists');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.usersRepository.findOneBy({ username });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const payload: JwtPayload = { username };
+            const accessToken: string = await this.jwtService.sign(payload);
+            return { accessToken };
+        } else {
+            throw new UnauthorizedException('Please check your login credentials');
+        }
+    }
+}
