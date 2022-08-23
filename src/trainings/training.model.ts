f07fd2eca91ec714e6b607eb37d9968453f14c1d
Hubert <hubert.zajac92@gmail.com>
2022-08-23 08:16:01 +0200

    Create basic controller paths and methods

---
+++ src/trainings/training.model.ts
@@ -0,0 +1,15 @@
+export interface Exercise {
+    id: string;
+    title: string;
+    date: Date;
+    exerciseName: string;
+    reps: string;
+    weights: string;
+
+}
+
+export type Training = Exercise[];
+
+export type DeleteTrainingResponse = {
+    isSuccess: true,
+}
--- src/trainings/trainings.controller.ts
+++ src/trainings/trainings.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Training } from './training.model';
 import { TrainingsService } from './trainings.service';
 
 @Controller('trainings')
@@ -8,6 +9,49 @@ export class TrainingsController {
     @Get('/')
     getAllTrainings(): Promise<Training> {
         return this.trainingsService.getAllTrainings(user);
+    }
+
+    @Get('/:title/:date')
+    getTrainingByTitleAndDate(
+        @Param('title') title: string,
+        @Param('date') date: Date,
+
+    ): Promise<Training> {
+
+        return this.trainingsService.getTrainingByTitleAndDate(title, date);
+    }
+
+    @Get('/:id')
+    getExerciseById(
+        @Param('id') id: string,
+
+    ): Promise<Exercise> {
+        return this.trainingsService.getExerciseById(id);
+    }
+
+    @Post('/')
+    createExercise(
+        @Body() createExerciseDto: CreateExerciseDto,
+
+    ): Promise<Exercise> {
+        return this.trainingsService.createExercise(createExerciseDto);
+    }
+
+    @Delete('/:title/:date')
+    deleteTraining(
+        @Param('title') title: string,
+        @Param('date') date: Date,
+
+    ): Promise<DeleteTrainingResponse> {
+        return this.trainingsService.deleteExercise(title, date);
+    }
+
+    @Patch('/:title/:date')
+    editTraining(
+        @Body() editTrainingDto: EditExerciseDto,
+
+    ) {
+        return this.trainingsService.editExercise(editTrainingDto);
     }
 
 }
