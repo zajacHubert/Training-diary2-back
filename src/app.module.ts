d026b2f9bd6d9b030206ec7e94b8940ee0ef3087
Hubert <hubert.zajac92@gmail.com>
2022-08-24 17:52:24 +0200

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -30,7 +30,7 @@ import { configValidationSchema } from './config.schema';
           host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
           username: configService.get('DB_USERNAME'),
-          password: configService.get(pwd),
+          password: configService.get('DB_PASSWORD'),
           database: configService.get('DB_DATABASE'),
         };
       },
