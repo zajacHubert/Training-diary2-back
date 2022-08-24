342ec056b19c1d14030dcc2d1ac4b44dbe732eaf
Hubert <hubert.zajac92@gmail.com>
2022-08-24 14:23:33 +0200

    ifx

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -6,6 +6,7 @@ export const configValidationSchema = Joi.object({
     DB_HOST: Joi.string().required(),
     DB_PORT: Joi.number().default(3306).required(),
     DB_USERNAME: Joi.string().required(),
+    DB_PASSWORD: Joi.string().required(),
     DB_DATABASE: Joi.string().required(),
     JWT_SECRET: Joi.string().default('secretPassword').required(),
 });
